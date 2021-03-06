14b72997f1c788ded1bf3a6d63a890258d41620c
YanaGuzienko <69dusha69@ukr.net>
2021-03-06 02:16:03 +0200

    добавляет разметку

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
+import url from './js/config';
+import debounce from 'lodash.debounce';
+import countriesTmp from './templates/countries.hbs';
+import countriesListTmp from './templates/countries-list.hbs';
 import './styles.css';
+
+const { BASE_URL } = url;
+
+const liRef = document.querySelector('#list');
+const inputRef = document.querySelector('.input-text');
+
+// function fetchCountries(e) {
+//   const name = e.target.value.toLowerCase();
+//   console.log(name);
+//   fetch(`${BASE_URL}${name}`)
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(countries => {
+//       countries.forEach(country => {
+//         liRef.insertAdjacentHTML(
+//           'afterbegin',
+//           `<h2>${country.name}</h2><li>${country.capital}</li><li>${country.population}</li><li>${country.languages}</li><img src="${country.flag}" width="40" height="40"/>`,
+//         );
+//         console.log(country);
+//       });
+
+//       console.log(countries);
+//     });
+// }
+
+function fetchCountries(e) {
+  const name = e.target.value.toLowerCase();
+  console.log(name);
+  fetch(`${BASE_URL}${name}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(countries => {
+      console.log(countries.length);
+      countCountry(countries);
+      // const result = countries.map(country => countriesTmp(country)).join('');
+      // liRef.innerHTML = countriesTmp(countries);
+      //   liRef.insertAdjacentHTML(
+      //   'beforeend',
+      //   countriesTmp(countries),
+      // );
+      // console.log(result);
+    });
+  // .then(countries => {
+  //   liRef.insertAdjacentHTML('beforeend', countries);
+  // });
+}
+// fetchCountries();
+inputRef.addEventListener('input', debounce(fetchCountries, 500));
+
+function countCountry(countries) {
+  if (countries.length >= 2 && countries.length <= 10) {
+    liRef.innerHTML = countriesListTmp(countries);
+  } else {
+    liRef.innerHTML = countriesTmp(countries);
+  }
+}
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+// const BASE_URL = 'https://restcountries.eu/rest/v2/';
+
+export default {
+  BASE_URL: 'https://restcountries.eu/rest/v2/name/',
+};
