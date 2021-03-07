01bcf3c23f782fd18baaf11c7a37db1dcb0c9d64
YanaGuzienko <69dusha69@ukr.net>
2021-03-08 00:37:50 +0200

    добавляет решение

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import url from './js/config';
 import debounce from 'lodash.debounce';
 import countriesTmp from './templates/countries.hbs';
 import countriesListTmp from './templates/countries-list.hbs';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import { error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
 import './styles.css';
 
 const { BASE_URL } = url;
@@ -9,55 +13,32 @@ const { BASE_URL } = url;
 const liRef = document.querySelector('#list');
 const inputRef = document.querySelector('.input-text');
 
-// function fetchCountries(e) {
-//   const name = e.target.value.toLowerCase();
-//   console.log(name);
-//   fetch(`${BASE_URL}${name}`)
-//     .then(response => {
-//       return response.json();
-//     })
-//     .then(countries => {
-//       countries.forEach(country => {
-//         liRef.insertAdjacentHTML(
-//           'afterbegin',
-//           `<h2>${country.name}</h2><li>${country.capital}</li><li>${country.population}</li><li>${country.languages}</li><img src="${country.flag}" width="40" height="40"/>`,
-//         );
-//         console.log(country);
-//       });
-
-//       console.log(countries);
-//     });
-// }
+defaultModules.set(PNotifyMobile, {});
 
 function fetchCountries(e) {
   const name = e.target.value.toLowerCase();
-  console.log(name);
+
   fetch(`${BASE_URL}${name}`)
     .then(response => {
       return response.json();
     })
     .then(countries => {
-      console.log(countries.length);
-      countCountry(countries);
-      // const result = countries.map(country => countriesTmp(country)).join('');
-      // liRef.innerHTML = countriesTmp(countries);
-      //   liRef.insertAdjacentHTML(
-      //   'beforeend',
-      //   countriesTmp(countries),
-      // );
-      // console.log(result);
-    });
-  // .then(countries => {
-  //   liRef.insertAdjacentHTML('beforeend', countries);
-  // });
+      renderCountry(countries);
+    })
+    .catch(console.log('Oops..An error occured, please try again'));
 }
-// fetchCountries();
-inputRef.addEventListener('input', debounce(fetchCountries, 500));
 
-function countCountry(countries) {
+inputRef.addEventListener('input', debounce(fetchCountries, 1000));
+
+function renderCountry(countries) {
   if (countries.length >= 2 && countries.length <= 10) {
     liRef.innerHTML = countriesListTmp(countries);
-  } else {
+  } else if (countries.length === 1) {
     liRef.innerHTML = countriesTmp(countries);
+    inputRef.value = '';
+  } else if (countries.length > 10) {
+    error({
+      text: 'Too many matches found.Please enter a more specific query!',
+    });
   }
 }
--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,3 @@
-// const BASE_URL = 'https://restcountries.eu/rest/v2/';
-
 export default {
   BASE_URL: 'https://restcountries.eu/rest/v2/name/',
 };
